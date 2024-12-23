22b516138481d3f8944814ccc05fd53558ece151
avitalpas <avitalpas@outlook.com>
2024-12-23 23:02:34 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
+
+    
     </div>
   );
 }
