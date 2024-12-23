9d7b96b0463aa3fc96990c0202922786e505092b
avitalpas <avitalpas@outlook.com>
2024-12-23 23:08:53 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import { Home } from "./comp/Home.js";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
-  </BrowserRouter>
-
-    
-    </div>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
   );
 }
 
